09cabb96ab594397c37081f1fc25f1897e974abd
Juandavid Reyes <jd.reyespaez@gmail.com>
2026-06-26 23:01:39 -0500

    Rebrand header to juandavid.xyz, rename About to /ahora, simplify mobile nav
    - SITE_TITLE: "Juan David" -> "juandavid.xyz"
    - /about -> /ahora, nav link "Sobre mí" -> "Ahora"
    - Hide header brand on mobile (<=600px), nav centered

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Juan David';
+export const SITE_TITLE = 'juandavid.xyz';
 export const SITE_DESCRIPTION = 'Escritos de Juan David Reyes Páez.';
